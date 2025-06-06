f48f72a29a65f94321c12bde6d98cdc55f5b9a43
LuisinaBender <luisinabender@hotmail.com>
2025-06-06 14:05:51 -0300

    Remove unused components and clean up App.jsx; add workspace state and document layout files.

--- src/App.jsx
+++ src/App.jsx
@@ -15,31 +15,10 @@ function App() {
       <Logo />
       <ItemListContainer saludo='Bienvenidos a tu solucion online'/>
       <ItemCount stock ={10}/>
-
-      
     </>
   );
 }
 
 export default App;
-
-
-  
-  /*const styles = {
-  backgroundColor:'yellow',
-  padding:'1rem',
-  color:'black',
-  fontWeight:'bold'
-  }
-  const saludar = () =>{
-  alert('Hola!')
-  }
-  return (
-    <>
-      <Contador/>
-      <ButtonMultiuso texto={'Mi boton alerta'} styles={styles} handler={saludar}/>
-      <ButtonMultiuso texto='Otro boton' cssStyle='mi-boton'/>
-    </>
-  )*/
 
 
